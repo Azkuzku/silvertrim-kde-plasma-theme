fbbcbf4fbf704219f6e76154f429e3d62ca0e3d0
aake <aake@aakekannettava.taskila>
2026-08-31 15:00:55 +0300

    Fixed install script

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
+mkdir ~/.local/share/plasma ~/.local/share/plasma/desktoptheme ~/.local/share/aurorae ~/.local/share/aurorae/themes/ ~/.local/share/color-schemes/ ~/.local/share/plasma/look-and-feel/
 echo "Copying color scheme..."
-cp -r color-scheme/Silvertrim.colors -t ~/.local/share/color-schemes/
+cp -rf color-scheme/Silvertrim.colors -t ~/.local/share/color-schemes/
 echo "Copying window decoration..."
-cp -r window-decoration/silvertrim -t ~/.local/share/aurorae/themes/
+cp -rf window-decoration/silvertrim -t ~/.local/share/aurorae/themes/
 echo "Copying desktop theme..."
-cp -r plasma-theme/silvertrim-plasma -t ~/.local/share/plasma/desktoptheme/
+cp -rf plasma-theme/silvertrim-plasma -t ~/.local/share/plasma/desktoptheme/
 echo "Copying global theme..."
-cp -r global-theme/silvertrim-plasma -t ~/.local/share/plasma/look-and-feel/
+cp -rf global-theme/silvertrim-plasma -t ~/.local/share/plasma/look-and-feel/
 echo "Done!"

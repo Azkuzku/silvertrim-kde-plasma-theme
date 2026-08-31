478235b71ed368a51d3d3696bb6e40f1bd219ab1
aake <aake@aukusti.taskila>
2026-08-31 13:55:15 +0300

    Added the theme files to the repo

---
+++ install.sh
@@ -0,0 +1,9 @@
+echo "Copying color scheme..."
+cp -r color-scheme/Silvertrim.colors -t ~/.local/share/color-schemes/
+echo "Copying window decoration..."
+cp -r window-decoration/silvertrim -t ~/.local/share/aurorae/themes/
+echo "Copying desktop theme..."
+cp -r plasma-theme/silvertrim-plasma -t ~/.local/share/plasma/desktoptheme/
+echo "Copying global theme..."
+cp -r global-theme/silvertrim-plasma -t ~/.local/share/plasma/look-and-feel/
+echo "Done!"

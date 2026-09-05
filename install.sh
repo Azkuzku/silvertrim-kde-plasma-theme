8080aebc42552ec0bff382e92b615ff846e771d6
aake <aake@aksu.taskila-kannettava>
2026-09-05 17:27:08 +0300

    Updated script, fixed panel background styling

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
-mkdir ~/.local/share/plasma ~/.local/share/plasma/desktoptheme ~/.local/share/aurorae ~/.local/share/aurorae/themes/ ~/.local/share/color-schemes/ ~/.local/share/plasma/look-and-feel/
+mkdir ~/.local/share/plasma ~/.local/share/plasma/desktoptheme ~/.local/share/aurorae ~/.local/share/aurorae/themes/ ~/.local/share/color-schemes/ ~/.local/share/plasma/look-and-feel/ ~/.local/share/icons
 echo "Copying color scheme..."
 cp -rf color-scheme/Silvertrim.colors -t ~/.local/share/color-schemes/
 echo "Copying window decoration..."
 cp -rf window-decoration/silvertrim -t ~/.local/share/aurorae/themes/
 echo "Copying desktop theme..."
 cp -rf plasma-theme/silvertrim-plasma -t ~/.local/share/plasma/desktoptheme/
+echo "Copying icon theme..."
+cp -rf icon-theme/silvertrim-icons -t ~/.local/share/icons/
 echo "Copying global theme..."
 cp -rf global-theme/silvertrim-plasma -t ~/.local/share/plasma/look-and-feel/
 echo "Done!"
